4ba31ae23a74eea6dff205a04ffd0d145f417a8d
TetianaBonar <tetianafed@bigmir.net>
2016-05-17 12:37:52 +0100

    Add files via upload

--- Javascript_3-4/script.js
+++ Javascript_3-4/script.js
@@ -1,29 +1,87 @@
-var body = document.getElementsByTagName("body")[0];
-var test = document.createElement('div');
+var test = {
+ createElement: function(params) {
+  console.log('params', params);
+  var element = document.createElement(params.tagName);
 
+   if(params.inputType) {
+     element.setAttribute('type', params.inputType);
+   }
 
-question = {
-text: "What is the question?",
-answers: [
-  {answer: "Answer is 1",
-   flag: true},
-  {answer: "Answer is 2",
-    flag: false},
-  {answer: "Answer is 3",
-    flag: false}
-],
- getQuestion: function (name) {
- var list = "";
- for (i = 0; i < this.answers.length; i++) {
-   list += '<li><input type = "checkbox" name = "myname">' + this.answers[i].answer + ' </li>'
+   if(params.className) {
+     element.className = params.className;
+   }
+
+   if(params.content) {
+     element.innerHTML = params.content;
+   }
+
+   if(params.parentElement) {
+     params.parentElement.appendChild(element);
+   }
+
+   return element;
+ },
+
+ generateQuestions: function(questionsAmount, answersAmount) {
+
+  for (var i = 0; i < questionsAmount; i++) {
+
+    this.createElement ({
+      tagName:"h2",
+      content: "Question #" + (i + 1),
+      parentElement: form
+    });
+
+    for (var j = 0; j < answersAmount; j++) {
+      var label = this.createElement ({
+        tagName: 'label',
+        content: 'Answer #' + (j + 1),
+        parentElement: form
+      });
+
+       var checkbox = this.createElement ({
+         tagName: 'input',
+         inputType: 'checkbox'
+    });
+
+    label.insertAdjacentElement('afterBegin', checkbox);
+    }
+  };
  }
-
-  return '<h1>' + this.answer + name + '</h1><ul>' + list + '</ul>';
- }
-};
-
-body.appendChild(test);
-
-for (i = 0; i < 4; i++) {
- test.innerHTML = test.innerHTML + question.getQuestion(i);
 }
+
+
+var body = document.getElementsByTagName("body")[0];
+
+var wrapper = document.createElement('div');
+wrapper.className = "wrapper";
+body.appendChild(wrapper);
+
+test.createElement({
+  tagName: 'h1',
+  content: 'Programming test',
+  parentElement: wrapper
+});
+
+
+var form = test.createElement ({
+  tagName: 'form',
+  parentElement: wrapper
+});
+
+test.generateQuestions(3, 3);
+
+test.createElement ({
+  tagName: 'input',
+  className: 'check',
+  inputType: 'submit',
+  content: 'Check the answers',
+  parentElement: form
+});
+
+
+
+// var submit = document.createElement('button');
+// var text = document.createTextNode('Check my results');
+// submit.appendChild(text);
+// wrapper.appendChild(submit);
